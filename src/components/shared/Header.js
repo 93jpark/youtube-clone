1e5a79bf9031f01679e41bef5b71c692339e0f2a
Joonwoo Park <93jpark@gmail.com>
2021-12-29 17:06:45 +0900

    day8 done

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,10 +1,27 @@
 import styles from './Header.module.css';
+import youtube_logo from '../../data/youtube_logo.png';
+import { FiMenu } from 'react-icons/fi';
+import { IoSearchOutline } from 'react-icons/io5';
+import { HiOutlineDotsVertical } from 'react-icons/hi';
+import { BsGrid3X3Gap } from 'react-icons/bs';
+
 
 const Header = () => {
     return (
-        <>
-            <div>헤더!</div>
-        </>
+        <div className={styles.header}>
+            <div className={styles.tab}>
+                <FiMenu className={styles.icon} />
+                <img src={youtube_logo} alt="Logo" className={styles.logo} />
+            </div>
+            <div className={styles['center-tab']}>
+                <input className={styles.input} />
+                <IoSearchOutline className={styles['search-icon']}/>
+            </div>
+            <div className={styles.tab}>
+                <BsGrid3X3Gap className={styles.icon} />
+                <HiOutlineDotsVertical className={styles.icon} />
+            </div>
+        </div>
     )
 }
 
