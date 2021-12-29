e0f4630507413c6e52ff6bc544c6ef03e321d1c0
Joonwoo Park <93jpark@gmail.com>
2021-12-29 18:09:58 +0900

    day 11 done

--- src/components/pages/Explore.js
+++ src/components/pages/Explore.js
@@ -1,9 +1,10 @@
 import Layout from '../shared/Layout';
+import ContentsLayout from '../shared/ContentsLayout';
 
 const  Explore = () => {
     return (
         <Layout activeMenu="explore">
-            <div>* 탐색</div>
+            <ContentsLayout>컨텐츠랍니다</ContentsLayout>
         </Layout>
     )
 }
---
+++ src/components/shared/ContentsLayout.js
@@ -0,0 +1,11 @@
+import styles from './ContentsLayout.module.css';
+
+const ContentsLayout = ({ children }) => {
+    return (
+        <div className={styles.layout}>
+            <div className={styles.container}>{children}</div>
+        </div>
+    )
+}
+
+export default ContentsLayout;
